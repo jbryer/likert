e4c733a8c995dbdb12829fc204caf26ed600f370
Jason Bryer <jason@bryer.org>
2012-10-16 21:15:49 -0400

    Fixed documentation issue with pisana.

--- R/likert-package.R
+++ R/likert-package.R
@@ -8,12 +8,14 @@
 #' @import reshape ggplot2 tools xtable
 NULL
 
+#' Programme of International Student Assessment
+#' 
 #' North American (i.e. Canada, Mexico, and United States) results from the 2009
 #' Programme of International Student Assessment (PISA)
 #' as provided by the Organization for Economic Co-operation and Development (OECD).
 #' See \url{http://www.pisa.oecd.org/} for more information including the code book.
 #'
-#' @name pisa
+#' @name pisana
 #' @docType data
 #' @format a data frame 66,690 ovservations of 437 variables from North America.
 #' @source Organization for Economic Co-operation and Development
