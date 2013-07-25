67475fc4376f3dd1b02a99fba0ce64cf26e06b48
Jason Bryer <jason@bryer.org>
2013-07-24 20:11:24 -0400

    Replaced cbind with merge to fix a bug where sometimes the values would not match up. Thanks to Vasco for reporting this bug.

--- R/likert.R
+++ R/likert.R
@@ -25,60 +25,63 @@
 #' @seealso summary.likert
 #' @examples
 #' data(pisaitems)
-#' items29 = pisaitems[,substr(names(pisaitems), 1,5) == 'ST25Q']
-#' names(items29) = c("Magazines", "Comic books", "Fiction", 
+#' items29 <- pisaitems[,substr(names(pisaitems), 1,5) == 'ST25Q']
+#' names(items29) <- c("Magazines", "Comic books", "Fiction", 
 #'                    "Non-fiction books", "Newspapers")
-#' l29 = likert(items29)
+#' l29 <- likert(items29)
 #' summary(l29)
 #' plot(l29)
 likert <- function(items, grouping=NULL, nlevels=length(levels(items[,1]))) {
-	lowrange = 1 : ceiling(nlevels / 2 - nlevels %% 2)
-	highrange = ceiling(nlevels / 2 + 1 ) : nlevels
+	lowrange <- 1 : ceiling(nlevels / 2 - nlevels %% 2)
+	highrange <- ceiling(nlevels / 2 + 1 ) : nlevels
 	
 	results <- data.frame()
 	if(!is.null(grouping)) {
-		results = data.frame(
+		results <- data.frame(
 			Group = rep(unique(grouping), each=nlevels),
 			Response = rep(1:nlevels, length(unique(grouping)))
 			)
 		for(i in 1:ncol(items)) {
-			t = as.data.frame(table(grouping, items[,i]))
-			t = cast(t, Var2 ~ grouping, value='Freq', add.missing=TRUE)
-			t = apply(t, 2, FUN=function(x) { x / sum(x) * 100 } )
-			t = melt(t)
-			results = cbind(results, t[,3])
+			t <- as.data.frame(table(grouping, items[,i]))
+			t <- cast(t, Var2 ~ grouping, value='Freq', add.missing=TRUE)
+			t <- apply(t, 2, FUN=function(x) { x / sum(x) * 100 } )
+			t <- melt(t)
+			results <- merge(results, t, 
+							 by.x=c('Group','Response'), by.y=c('X2','X1'), 
+							 all.x=TRUE)
 		}
 		
-		names(results)[3:ncol(results)] = names(items)
+		names(results)[3:ncol(results)] <- names(items)
 		
-		results$Response = factor(results$Response, levels=1:nlevels, 
+		results$Response <- factor(results$Response, levels=1:nlevels, 
 								  labels=levels(items[,i]))
-		results = melt(results, id=c('Group', 'Response'))
-		results = cast(results, Group + variable ~ Response)
-		results = as.data.frame(results)
-		names(results)[2] = 'Item'
+		results <- melt(results, id=c('Group', 'Response'))
+		results <- cast(results, Group + variable ~ Response)
+		results <- as.data.frame(results)
+		names(results)[2] <- 'Item'
 	} else {
-		results = data.frame(Response=1:nlevels)
-		means = numeric()
-		sds = numeric()
+		results <- data.frame(Response=1:nlevels)
+		means <- numeric()
+		sds <- numeric()
 		for(i in 1:ncol(items)) {
-			t = table(items[,i])
-			t = (t / sum(t) * 100)
-			means[i] = mean(as.numeric(items[,i]), na.rm=TRUE)
-			sds[i] = sd(as.numeric(items[,i]), na.rm=TRUE)
-			results = cbind(results, as.data.frame(t)[2])
-			names(results)[ncol(results)] = names(items)[i]
+			t <- table(items[,i])
+			t <- (t / sum(t) * 100)
+			means[i] <- mean(as.numeric(items[,i]), na.rm=TRUE)
+			sds[i] <- sd(as.numeric(items[,i]), na.rm=TRUE)
+			results <- merge(results, as.data.frame(t),
+							 by.x='Response', by.y='Var1', all.x=TRUE)
+			names(results)[ncol(results)] <- names(items)[i]
 		}
-		results = as.data.frame(t(results))
-		names(results) = levels(items[,1])
-		results = results[2:nrow(results),]
+		results <- as.data.frame(t(results))
+		names(results) <- levels(items[,1])
+		results <- results[2:nrow(results),]
 		
-		results = cbind(row.names(results), results)
-		names(results)[1] = 'Item'
-		row.names(results) = 1:nrow(results) 		
+		results <- cbind(row.names(results), results)
+		names(results)[1] <- 'Item'
+		row.names(results) <- 1:nrow(results) 		
 	}
 	
-	r = list(results=results, items=items, grouping=grouping, nlevels=nlevels)
-	class(r) = 'likert'
+	r <- list(results=results, items=items, grouping=grouping, nlevels=nlevels)
+	class(r) <- 'likert'
 	return(r)
 }
