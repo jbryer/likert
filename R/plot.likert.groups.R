ca1521b52b2387a711f6d851fb86ca272d97d2e8
Stian Håklev <shaklev@gmail.com>
2013-09-01 16:12:27 -0400

    Passing options on to likert in plot.likert.groups, enabling grouping=

--- R/plot.likert.groups.R
+++ R/plot.likert.groups.R
@@ -1,7 +1,7 @@
 #'Plots a group of Likert-items, as stored by likert_store_groups. groups= can be one group, indexed by title or by number
 #'or a list of groups. all=T prints all groups. 
 
-plot_likert_groups <- function(db, all=F, groups=NA) {
+plot_likert_groups <- function(db, all=F, groups=NA, ...) {
   attrgroups <- attr(db, "likert.groups")
   
   if(is.null(attrgroups)) {
@@ -18,7 +18,7 @@ plot_likert_groups <- function(db, all=F, groups=NA) {
   
   for(e in groups) {
     group <- attrgroups[[e]]
-    ligroup <- likert(db[,group])
+    ligroup <- likert(db[,group], ...)
     print(plot(ligroup) + ggtitle(names(attrgroups[e])) )
   }
   
