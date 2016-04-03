fffa8522a9866b19e55db6893e6d295c20af9583
m_dev <m.devlin.dmu@gmail.com>
2016-04-03 03:18:49 +0100

    remove comments

--- R/likert.R
+++ R/likert.R
@@ -59,11 +59,11 @@ likert <- function(items, summary,
 					  nlevels=(ncol(summary)-1),
 					  levels=names(summary[,2:ncol(summary)]))
 		} else {
-			r <- list(results=cbind(Group=grouping, summary[,-1]), ## split Group and summary 
+			r <- list(results=cbind(Group=grouping, summary[,-1]), 
 					  items=NULL, 
 					  grouping=grouping, 
-					  nlevels=(ncol(summary)-2),  ## reduce nlevels
-					  levels=names(summary[,3:ncol(summary)])) ## reduce levels
+					  nlevels=(ncol(summary)-2),
+					  levels=names(summary[,3:ncol(summary)]))
 		}
 		class(r) <- 'likert'
 		return(r)
--- R/summary.likert.r
+++ R/summary.likert.r
@@ -44,13 +44,13 @@ summary.likert <- function(object, center=(object$nlevels-1)/2 + 1,
 			neutral <- NA
 		}
 		if(is.null(object$grouping)) {
-			lowCols <- startCol:floor(center + 0.5) ## insert floor
+			lowCols <- startCol:floor(center + 0.5)
 			if(length(lowCols) == 1) {
 				low <- results[,lowCols]
 			} else {
 				low <- apply(results[,lowCols], 1, sum)
 			}
-			highCols <- (floor(center)+startCol):ncol(results) ## insert floor, remove -0.5
+			highCols <- (floor(center)+startCol):ncol(results)
 			if(length(highCols) == 1) {
 				high <- results[,highCols]
 			} else {
@@ -63,13 +63,13 @@ summary.likert <- function(object, center=(object$nlevels-1)/2 + 1,
 								   mean=tmp[,1],
 								   sd=tmp[,2]  )
 		} else {
-			lowCols <- startCol:(ceiling(center) + 1) ## insert ceiling
+			lowCols <- startCol:(ceiling(center) + 1)
 			if(length(lowCols) == 1) {
 				low <- results[,lowCols]
 			} else {
 				low <- apply(results[,lowCols], 1, sum)
 			}
-			highCols <- (floor(center)+startCol):ncol(results) ## insert floor
+			highCols <- (floor(center)+startCol):ncol(results)
 			if(length(highCols) == 1) {
 				high <- results[,highCols]
 			} else {
